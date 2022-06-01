eef2e537c2c740318676e01a7727c2da08c18508
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2022-05-31 21:43:03 -0300

    Proxy

---
+++ Proxy/src/Image.java
@@ -0,0 +1,3 @@
+public abstract class Image {
+    public abstract void displayImage();
+}
---
+++ Proxy/src/ProxyImage.java
@@ -0,0 +1,15 @@
+public class ProxyImage extends Image {
+    private String filename;
+    private RealImage image;
+
+    public ProxyImage(String filename) {
+        this.filename = filename;
+    }
+
+    public void displayImage() {
+        if (image == null) {
+            image = new RealImage(filename);
+        }
+        image.displayImage();
+    }
+}
